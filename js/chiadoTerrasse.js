9ff9873130acc6d6ea2db16b7aacb7fc2c5e1d31
Netto Baptistella <netto.e.baptistella@gmail.com>
2021-10-25 22:09:37 +0100

    init

---
+++ js/chiadoTerrasse.js
@@ -0,0 +1,309 @@
+function entrada() {
+    document.getElementById("mEntradas").style.display = 'block';
+    document.getElementById("mCarnes").style.display = 'none';
+    document.getElementById("mPeixe").style.display = 'none';
+    document.getElementById("mMassa").style.display = 'none';
+    document.getElementById("mCrepes").style.display = 'none';
+    document.getElementById("mOmeletes").style.display = 'none';
+    document.getElementById("mSaladas").style.display = 'none';
+    document.getElementById("mTostas").style.display = 'none';
+    document.getElementById("btnClose").style.display = 'block';
+    document.getElementById("fotoFundo").style.display = 'none';
+    document.getElementById("mBebidas1").style.display = 'none';
+    document.getElementById("mBebidas2").style.display = 'none';
+    document.getElementById("mBebidas3").style.display = 'none';
+    document.getElementById("mBebidas4").style.display = 'none';
+    document.getElementById("mSobremesa1").style.display = 'none';
+    document.getElementById("mSobremesa2").style.display = 'none';
+    document.getElementById("mSobremesa3").style.display = 'none';
+    document.getElementById("mSobremesa4").style.display = 'none';
+    document.getElementById("s1").style.display = 'none';
+    document.getElementById("s2").style.display = 'none';
+    document.getElementById("s3").style.display = 'none';
+    document.getElementById("b1").style.display = 'none';
+    document.getElementById("b2").style.display = 'none';
+    document.getElementById("b3").style.display = 'none';
+}
+
+function carne() {
+    document.getElementById("mEntradas").style.display = 'none';
+    document.getElementById("mCarnes").style.display = 'block';
+    document.getElementById("mPeixe").style.display = 'none';
+    document.getElementById("mMassa").style.display = 'none';
+    document.getElementById("mCrepes").style.display = 'none';
+    document.getElementById("mOmeletes").style.display = 'none';
+    document.getElementById("mSaladas").style.display = 'none';
+    document.getElementById("mTostas").style.display = 'none';
+    document.getElementById("btnClose").style.display = 'block';
+    document.getElementById("fotoFundo").style.display = 'none';
+    document.getElementById("mBebidas1").style.display = 'none';
+    document.getElementById("mBebidas2").style.display = 'none';
+    document.getElementById("mBebidas3").style.display = 'none';
+    document.getElementById("mBebidas4").style.display = 'none';
+    document.getElementById("mSobremesa1").style.display = 'none';
+    document.getElementById("mSobremesa2").style.display = 'none';
+    document.getElementById("mSobremesa3").style.display = 'none';
+    document.getElementById("mSobremesa4").style.display = 'none';
+    document.getElementById("s1").style.display = 'none';
+    document.getElementById("s2").style.display = 'none';
+    document.getElementById("s3").style.display = 'none';
+    document.getElementById("b1").style.display = 'none';
+    document.getElementById("b2").style.display = 'none';
+    document.getElementById("b3").style.display = 'none';
+
+}
+
+function peixe() {
+    document.getElementById("mEntradas").style.display = 'none';
+    document.getElementById("mCarnes").style.display = 'none';
+    document.getElementById("mPeixe").style.display = 'block';
+    document.getElementById("mMassa").style.display = 'none';
+    document.getElementById("mCrepes").style.display = 'none';
+    document.getElementById("mOmeletes").style.display = 'none';
+    document.getElementById("mSaladas").style.display = 'none';
+    document.getElementById("mTostas").style.display = 'none';
+    document.getElementById("btnClose").style.display = 'block';
+    document.getElementById("fotoFundo").style.display = 'none';
+    document.getElementById("mBebidas1").style.display = 'none';
+    document.getElementById("mBebidas2").style.display = 'none';
+    document.getElementById("mBebidas3").style.display = 'none';
+    document.getElementById("mBebidas4").style.display = 'none';
+    document.getElementById("mSobremesa1").style.display = 'none';
+    document.getElementById("mSobremesa2").style.display = 'none';
+    document.getElementById("mSobremesa3").style.display = 'none';
+    document.getElementById("mSobremesa4").style.display = 'none';
+    document.getElementById("s1").style.display = 'none';
+    document.getElementById("s2").style.display = 'none';
+    document.getElementById("s3").style.display = 'none';
+    document.getElementById("b1").style.display = 'none';
+    document.getElementById("b2").style.display = 'none';
+    document.getElementById("b3").style.display = 'none';
+
+}
+
+function massas() {
+    document.getElementById("mEntradas").style.display = 'none';
+    document.getElementById("mCarnes").style.display = 'none';
+    document.getElementById("mPeixe").style.display = 'none';
+    document.getElementById("mMassa").style.display = 'block';
+    document.getElementById("mCrepes").style.display = 'none';
+    document.getElementById("mOmeletes").style.display = 'none';
+    document.getElementById("mSaladas").style.display = 'none';
+    document.getElementById("mTostas").style.display = 'none';
+    document.getElementById("btnClose").style.display = 'block';
+    document.getElementById("fotoFundo").style.display = 'none';
+    document.getElementById("mBebidas1").style.display = 'none';
+    document.getElementById("mBebidas2").style.display = 'none';
+    document.getElementById("mBebidas3").style.display = 'none';
+    document.getElementById("mBebidas4").style.display = 'none';
+    document.getElementById("mSobremesa1").style.display = 'none';
+    document.getElementById("mSobremesa2").style.display = 'none';
+    document.getElementById("mSobremesa3").style.display = 'none';
+    document.getElementById("mSobremesa4").style.display = 'none';
+    document.getElementById("s1").style.display = 'none';
+    document.getElementById("s2").style.display = 'none';
+    document.getElementById("s3").style.display = 'none';
+    document.getElementById("b1").style.display = 'none';
+    document.getElementById("b2").style.display = 'none';
+    document.getElementById("b3").style.display = 'none';
+
+}
+
+function crepes() {
+    document.getElementById("mEntradas").style.display = 'none';
+    document.getElementById("mCarnes").style.display = 'none';
+    document.getElementById("mPeixe").style.display = 'none';
+    document.getElementById("mMassa").style.display = 'none';
+    document.getElementById("mCrepes").style.display = 'block';
+    document.getElementById("mOmeletes").style.display = 'none';
+    document.getElementById("mSaladas").style.display = 'none';
+    document.getElementById("mTostas").style.display = 'none';
+    document.getElementById("btnClose").style.display = 'block';
+    document.getElementById("fotoFundo").style.display = 'none';
+    document.getElementById("mBebidas1").style.display = 'none';
+    document.getElementById("mBebidas2").style.display = 'none';
+    document.getElementById("mBebidas3").style.display = 'none';
+    document.getElementById("mBebidas4").style.display = 'none';
+    document.getElementById("mSobremesa1").style.display = 'none';
+    document.getElementById("mSobremesa2").style.display = 'none';
+    document.getElementById("mSobremesa3").style.display = 'none';
+    document.getElementById("mSobremesa4").style.display = 'none';
+    document.getElementById("s1").style.display = 'none';
+    document.getElementById("s2").style.display = 'none';
+    document.getElementById("s3").style.display = 'none';
+    document.getElementById("b1").style.display = 'none';
+    document.getElementById("b2").style.display = 'none';
+    document.getElementById("b3").style.display = 'none';
+
+}
+
+function omeletes() {
+    document.getElementById("mEntradas").style.display = 'none';
+    document.getElementById("mCarnes").style.display = 'none';
+    document.getElementById("mPeixe").style.display = 'none';
+    document.getElementById("mMassa").style.display = 'none';
+    document.getElementById("mCrepes").style.display = 'none';
+    document.getElementById("mOmeletes").style.display = 'block';
+    document.getElementById("mSaladas").style.display = 'none';
+    document.getElementById("mTostas").style.display = 'none';
+    document.getElementById("btnClose").style.display = 'block';
+    document.getElementById("fotoFundo").style.display = 'none';
+    document.getElementById("mBebidas1").style.display = 'none';
+    document.getElementById("mBebidas2").style.display = 'none';
+    document.getElementById("mBebidas3").style.display = 'none';
+    document.getElementById("mBebidas4").style.display = 'none';
+    document.getElementById("mSobremesa1").style.display = 'none';
+    document.getElementById("mSobremesa2").style.display = 'none';
+    document.getElementById("mSobremesa3").style.display = 'none';
+    document.getElementById("mSobremesa4").style.display = 'none';
+    document.getElementById("s1").style.display = 'none';
+    document.getElementById("s2").style.display = 'none';
+    document.getElementById("s3").style.display = 'none';
+    document.getElementById("b1").style.display = 'none';
+    document.getElementById("b2").style.display = 'none';
+    document.getElementById("b3").style.display = 'none';
+
+}
+
+function saladas() {
+    document.getElementById("mEntradas").style.display = 'none';
+    document.getElementById("mCarnes").style.display = 'none';
+    document.getElementById("mPeixe").style.display = 'none';
+    document.getElementById("mMassa").style.display = 'none';
+    document.getElementById("mCrepes").style.display = 'none';
+    document.getElementById("mOmeletes").style.display = 'none';
+    document.getElementById("mSaladas").style.display = 'block';
+    document.getElementById("mTostas").style.display = 'none';
+    document.getElementById("btnClose").style.display = 'block';
+    document.getElementById("fotoFundo").style.display = 'none';
+    document.getElementById("mBebidas1").style.display = 'none';
+    document.getElementById("mBebidas2").style.display = 'none';
+    document.getElementById("mBebidas3").style.display = 'none';
+    document.getElementById("mBebidas4").style.display = 'none';
+    document.getElementById("mSobremesa1").style.display = 'none';
+    document.getElementById("mSobremesa2").style.display = 'none';
+    document.getElementById("mSobremesa3").style.display = 'none';
+    document.getElementById("mSobremesa4").style.display = 'none';
+    document.getElementById("s1").style.display = 'none';
+    document.getElementById("s2").style.display = 'none';
+    document.getElementById("s3").style.display = 'none';
+    document.getElementById("b1").style.display = 'none';
+    document.getElementById("b2").style.display = 'none';
+    document.getElementById("b3").style.display = 'none';
+
+}
+
+function tostas() {
+    document.getElementById("mEntradas").style.display = 'none';
+    document.getElementById("mCarnes").style.display = 'none';
+    document.getElementById("mPeixe").style.display = 'none';
+    document.getElementById("mMassa").style.display = 'none';
+    document.getElementById("mCrepes").style.display = 'none';
+    document.getElementById("mOmeletes").style.display = 'none';
+    document.getElementById("mSaladas").style.display = 'none';
+    document.getElementById("mTostas").style.display = 'block';
+    document.getElementById("btnClose").style.display = 'block';
+    document.getElementById("fotoFundo").style.display = 'none';
+    document.getElementById("mBebidas1").style.display = 'none';
+    document.getElementById("mBebidas2").style.display = 'none';
+    document.getElementById("mBebidas3").style.display = 'none';
+    document.getElementById("mBebidas4").style.display = 'none';
+    document.getElementById("mSobremesa1").style.display = 'none';
+    document.getElementById("mSobremesa2").style.display = 'none';
+    document.getElementById("mSobremesa3").style.display = 'none';
+    document.getElementById("mSobremesa4").style.display = 'none';
+    document.getElementById("s1").style.display = 'none';
+    document.getElementById("s2").style.display = 'none';
+    document.getElementById("s3").style.display = 'none';
+    document.getElementById("b1").style.display = 'none';
+    document.getElementById("b2").style.display = 'none';
+    document.getElementById("b3").style.display = 'none';
+
+}
+
+function bebidas() {
+    document.getElementById("mEntradas").style.display = 'none';
+    document.getElementById("mCarnes").style.display = 'none';
+    document.getElementById("mPeixe").style.display = 'none';
+    document.getElementById("mMassa").style.display = 'none';
+    document.getElementById("mCrepes").style.display = 'none';
+    document.getElementById("mOmeletes").style.display = 'none';
+    document.getElementById("mSaladas").style.display = 'none';
+    document.getElementById("mTostas").style.display = 'none';
+    document.getElementById("btnClose").style.display = 'block';
+    document.getElementById("fotoFundo").style.display = 'none';
+    document.getElementById("mBebidas1").style.display = 'block';
+    document.getElementById("mBebidas2").style.display = 'block';
+    document.getElementById("mBebidas3").style.display = 'block';
+    document.getElementById("mBebidas4").style.display = 'block';
+    document.getElementById("mSobremesa1").style.display = 'none';
+    document.getElementById("mSobremesa2").style.display = 'none';
+    document.getElementById("mSobremesa3").style.display = 'none';
+    document.getElementById("mSobremesa4").style.display = 'none';
+    document.getElementById("s1").style.display = 'none';
+    document.getElementById("s2").style.display = 'none';
+    document.getElementById("s3").style.display = 'none';
+    document.getElementById("b1").style.display = 'block';
+    document.getElementById("b2").style.display = 'block';
+    document.getElementById("b3").style.display = 'block';
+}
+
+function sobremesas() {
+    document.getElementById("mEntradas").style.display = 'none';
+    document.getElementById("mCarnes").style.display = 'none';
+    document.getElementById("mPeixe").style.display = 'none';
+    document.getElementById("mMassa").style.display = 'none';
+    document.getElementById("mCrepes").style.display = 'none';
+    document.getElementById("mOmeletes").style.display = 'none';
+    document.getElementById("mSaladas").style.display = 'none';
+    document.getElementById("mTostas").style.display = 'none';
+    document.getElementById("btnClose").style.display = 'block';
+    document.getElementById("fotoFundo").style.display = 'none';
+    document.getElementById("mBebidas1").style.display = 'none';
+    document.getElementById("mBebidas2").style.display = 'none';
+    document.getElementById("mBebidas3").style.display = 'none';
+    document.getElementById("mBebidas4").style.display = 'none';
+    document.getElementById("mSobremesa1").style.display = 'block';
+    document.getElementById("mSobremesa2").style.display = 'block';
+    document.getElementById("mSobremesa3").style.display = 'block';
+    document.getElementById("mSobremesa4").style.display = 'block';
+    document.getElementById("s1").style.display = 'block';
+    document.getElementById("s2").style.display = 'block';
+    document.getElementById("s3").style.display = 'block';
+    document.getElementById("b1").style.display = 'none';
+    document.getElementById("b2").style.display = 'none';
+    document.getElementById("b3").style.display = 'none';
+
+}
+
+
+
+
+function closeImg(){
+    document.getElementById("mEntradas").style.display = 'none';
+    document.getElementById("mCarnes").style.display = 'none';
+    document.getElementById("mPeixe").style.display = 'none';
+    document.getElementById("mMassa").style.display = 'none';
+    document.getElementById("mCrepes").style.display = 'none';
+    document.getElementById("mOmeletes").style.display = 'none';
+    document.getElementById("mSaladas").style.display = 'none';
+    document.getElementById("mTostas").style.display = 'none';
+    document.getElementById("btnClose").style.display = 'none';
+    document.getElementById("mBebidas1").style.display = 'none';
+    document.getElementById("mBebidas2").style.display = 'none';
+    document.getElementById("mBebidas3").style.display = 'none';
+    document.getElementById("mBebidas4").style.display = 'none';
+    document.getElementById("mSobremesa1").style.display = 'none';
+    document.getElementById("mSobremesa2").style.display = 'none';
+    document.getElementById("mSobremesa3").style.display = 'none';
+    document.getElementById("mSobremesa4").style.display = 'none';
+    document.getElementById("s1").style.display = 'none';
+    document.getElementById("s2").style.display = 'none';
+    document.getElementById("s3").style.display = 'none';
+    document.getElementById("b1").style.display = 'none';
+    document.getElementById("b2").style.display = 'none';
+    document.getElementById("b3").style.display = 'none';
+
+
+    document.getElementById("fotoFundo").style.display = 'block';
+}
